d2d29d0ddda860aba83f5cba6edd2b997f7bf587
Christian Balentine <christianbalentine47@gmail.com>
2020-03-30 20:53:29 -0400

    added objects for reservations and waitlists

---
+++ api.js
@@ -0,0 +1,19 @@
+const express = require('express')
+const app = express()
+
+let reservations = [
+    {name = "Bob",
+    phone = 555-555-5555,
+    email = "christian@email.com",
+    ID = "5"
+}
+];
+
+let waitlist = [
+    {name = "Hugh",
+    phone = 222-222-2222,
+    email = "heff@email.com",
+    ID = "96"
+}
+];
+
